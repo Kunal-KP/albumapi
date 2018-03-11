5fbf32e0187378d9ebab43c24512eba5db45d0b5
Kunal Panigrahi <kunal.panigrahi@atherenergy.com>
2018-03-11 14:02:29 +0530

    data update final

--- index.js
+++ index.js
@@ -14,28 +14,28 @@ const data = [
       "artist": "Kishore Kumar",
       "url": "https://www.amazon.in/Yaadein-Kishore-Kumar/dp/B0119KI7ZO",
       "image": "https://images-na.ssl-images-amazon.com/images/I/61Lso-bmSLL.jpg",
-      "thumbnail_image": "https://imgur.com/ArwexHM"
+      "thumbnail_image": "https://images-na.ssl-images-amazon.com/images/I/61Lso-bmSLL.jpg"
     },
     {
       "title": "American Idiot",
       "artist": "Green Day",
       "url": "https://www.amazon.in/American-Idiot-Green-Day/dp/B0002OERI0",
       "image": "https://images-na.ssl-images-amazon.com/images/I/71Z0rLIvpuL._SL1448_.jpg",
-      "thumbnail_image": ""
+      "thumbnail_image": "https://images-na.ssl-images-amazon.com/images/I/71Z0rLIvpuL._SL1448_.jpg"
     },
     {
       "title": "Doorie",
       "artist": "Atif Aslam",
       "url": "https://www.amazon.in/Doorie-CD-Atif-Aslam/dp/B075ZWJB9J",
       "image": "https://images-na.ssl-images-amazon.com/images/I/81bd2rue8SL._SL1482_.jpg",
-      "thumbnail_image": ""
+      "thumbnail_image": "https://images-na.ssl-images-amazon.com/images/I/81bd2rue8SL._SL1482_.jpg"
     },
     {
       "title": "Bollywood Heights",
       "artist": "Shreya Ghoshal",
       "url": "https://www.amazon.in/Bollywood-Heights-Shreya-Ghoshal/dp/B0088LJD2O/",
       "image": "https://images-na.ssl-images-amazon.com/images/I/8185kfVlriL._SL1500_.jpg",
-      "thumbnail_image": ""
+      "thumbnail_image": "https://images-na.ssl-images-amazon.com/images/I/8185kfVlriL._SL1500_.jpg"
     }
   ]
 App.get('/', (req, res) => res.send(data))
