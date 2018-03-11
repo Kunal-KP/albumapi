fd7dee15c4d05e998dc9707d9537dcc753c1e18b
Kunal Panigrahi <kunal.panigrahi@atherenergy.com>
2018-03-11 13:56:53 +0530

    data update

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ const App = express()
 
 const data = [
     {
+        "title": "Soulful Voice",
+        "artist": "Arijit Singh",
+        "url": "https://www.amazon.in/Soulful-Voice-Arijit-Singh/dp/B0753CL95Q",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/81oK70kbtUL._SL1500_.jpg",
+        "thumbnail_image": "https://imgur.com/a/li32N"
+      },    
+    {
       "title": "Yaadein",
       "artist": "Kishore Kumar",
-      "url": "https://www.amazon.in/Yaadein-Kishore-Kumar/dp/B0119KI7ZO/",
+      "url": "https://www.amazon.in/Yaadein-Kishore-Kumar/dp/B0119KI7ZO",
       "image": "https://images-na.ssl-images-amazon.com/images/I/61Lso-bmSLL.jpg",
       "thumbnail_image": "https://imgur.com/ArwexHM"
     },
     {
-      "title": "Soulful Voice",
-      "artist": "Arijit Singh",
-      "url": "https://www.amazon.in/Soulful-Voice-Arijit-Singh/dp/B0753CL95Q",
-      "image": "https://images-na.ssl-images-amazon.com/images/I/81oK70kbtUL._SL1500_.jpg",
-      "thumbnail_image": "https://imgur.com/a/li32N"
+      "title": "American Idiot",
+      "artist": "Green Day",
+      "url": "https://www.amazon.in/American-Idiot-Green-Day/dp/B0002OERI0",
+      "image": "https://images-na.ssl-images-amazon.com/images/I/71Z0rLIvpuL._SL1448_.jpg",
+      "thumbnail_image": ""
     },
     {
-      "title": "Speak Now",
-      "artist": "Taylor Swift",
-      "url": "https://www.amazon.com/Speak-Now-Taylor-Swift/dp/B003WTE886",
-      "image": "https://images-na.ssl-images-amazon.com/images/I/51vlGuX7%2BFL.jpg",
-      "thumbnail_image": "https://i.imgur.com/K3KJ3w4h.jpg"
+      "title": "Doorie",
+      "artist": "Atif Aslam",
+      "url": "https://www.amazon.in/Doorie-CD-Atif-Aslam/dp/B075ZWJB9J",
+      "image": "https://images-na.ssl-images-amazon.com/images/I/81bd2rue8SL._SL1482_.jpg",
+      "thumbnail_image": ""
     },
     {
-      "title": "Red",
-      "artist": "Taylor Swift",
-      "url": "https://www.amazon.com/Red-Taylor-Swift/dp/B008XNZMOU",
-      "image": "https://images-na.ssl-images-amazon.com/images/I/41j7-7yboXL.jpg",
-      "thumbnail_image": "https://i.imgur.com/K3KJ3w4h.jpg"
-    },
-    {
-      "title": "1989",
-      "artist": "Taylor Swift",
-      "url": "https://www.amazon.com/1989-Taylor-Swift/dp/B00MRHANNI",
-      "image": "https://images-na.ssl-images-amazon.com/images/I/717DWgRftmL._SX522_.jpg",
-      "thumbnail_image": "https://i.imgur.com/K3KJ3w4h.jpg"
+      "title": "Bollywood Heights",
+      "artist": "Shreya Ghoshal",
+      "url": "https://www.amazon.in/Bollywood-Heights-Shreya-Ghoshal/dp/B0088LJD2O/",
+      "image": "https://images-na.ssl-images-amazon.com/images/I/8185kfVlriL._SL1500_.jpg",
+      "thumbnail_image": ""
     }
   ]
 App.get('/', (req, res) => res.send(data))
